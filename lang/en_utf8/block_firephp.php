f148f3cc9afcedb9b281d87771e3fda2f9907453
Frank Ralf <webmaster@nakohdo.de>
2011-07-20 18:53:19 +0200

    switched quote characters to avoid parsing error

--- lang/en_utf8/block_firephp.php
+++ lang/en_utf8/block_firephp.php
@@ -2,6 +2,6 @@
 // English language file for block_firephp
 $string['firephp']      = 'FirePHP';
 $string['activate']     = 'Activate FirePHP';
-$string['activated']    = 'FirePHP is activated!';
-$string['deactivated']  = 'FirePHP is deactivated.';
+$string['activated']      = "<p style='background-color:yellow; color:red;'>FirePHP is activated!</p>Ensure your Firebug Net AND Console panels are enabled.";
+$string['deactivated']    = "<span style='color:gray;'>FirePHP is deactivated.</span>";
 $string['savechanges']  = 'Save changes';
